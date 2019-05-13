c6bfcb28743437050a559f9716d10e6e1621ee9f
alexSsedano <44229620+alexSsedano@users.noreply.github.com>
2019-05-13 13:17:50 +0200

    c

--- src/components/homeBody.vue
+++ src/components/homeBody.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="homebody" style="background-color: white">
+    <div class="homebody  " style="background-color: white">
 
         <div  class="img-fluid  d-block d-sm-none extrasmall" style="height: 150px; "><div class="container">
                 <div class="row align-items-center" >
@@ -52,24 +52,26 @@
                 </div>
             </div>
         </div>
-        <div >
+        <div>
             <div class="row " style="margin-top: 100px">
                 <div class="  col-sm-4 ">
-                    <h2 class="text-center" style="margin-top: 10px"><strong>¿ Cómo surge DR.GOU ?</strong></h2>
-                    <p class="text-center" style="margin-top: 10px">La idea de crear este grupo surge como respuesta a las diversas necesidades y demandas que el alumnado plantea a lo largo de los diferentes cursos académicos y de las distintas promociones que van pasando por la Universidad. Estas necesidades y demandas están presentes de forma general en este alumnado, tanto al comenzar su andadura universitaria como al finalizarla, y hacen que sea más complicado su proceso de adaptación a la Universidad.</p>
+                    <h2 class="text-center" style="margin: 10px"><strong>¿ Cómo surge DR.GOU ?</strong></h2>
+                    <p class="text-center" style="margin: 10px">La idea de crear este grupo surge como respuesta a las diversas necesidades y demandas que el alumnado plantea a lo largo de los diferentes cursos académicos y de las distintas promociones que van pasando por la Universidad. Estas necesidades y demandas están presentes de forma general en este alumnado, tanto al comenzar su andadura universitaria como al finalizarla, y hacen que sea más complicado su proceso de adaptación a la Universidad.</p>
                 </div>
                 <div class="  col-sm-4 ">
-                    <h2 class="text-center" style="margin-top: 10px"><strong>¿ Que es DR.GOU ?</strong></h2>
-                    <p class="text-center" style="margin-top: 10px">El GOU es un Grupo de Orientación Universitaria formado por un equipo de profesores/as, voluntarios/as y alumnos/as de diversas Facultades de la Universidad Málaga, que trabajan con el objetivo de ejercer una labor informativa y formativa en su alumnado durante todo el período académico.</p>
+                    <h2 class="text-center" style="margin: 10px"><strong>¿ Que es DR.GOU ?</strong></h2>
+                    <p class="text-center" style="margin: 10px">El GOU es un Grupo de Orientación Universitaria formado por un equipo de profesores/as, voluntarios/as y alumnos/as de diversas Facultades de la Universidad Málaga, que trabajan con el objetivo de ejercer una labor informativa y formativa en su alumnado durante todo el período académico.</p>
                 </div>
                 <div class="  col-sm-4 ">
-                    <h2 class="text-center" style="margin-top: 10px"><strong>¿ A quien atiende DR.GOU ?</strong></h2>
-                    <p class="text-center" style="margin-top: 10px">Cada facultad atendemos al su alumnado en particular, especialmente al alumnado de nueva entrada y a los que se encuentren en los últimos cursos de la formación universitaria.No obstante, el GOU está disponible para responder cuestiones de carácter general que puedan surgir a cualquier alumno/a de otras facultades de la UMA, ya que existen determinadas cuestiones que son de interés para el conjunto del alumnado universitario.</p>
+                    <h2 class="text-center" style="margin: 10px"><strong>¿ A quien atiende DR.GOU ?</strong></h2>
+                    <p class="text-center" style="margin: 10px">Cada facultad atendemos al su alumnado en particular, especialmente al alumnado de nueva entrada y a los que se encuentren en los últimos cursos de la formación universitaria.No obstante, el GOU está disponible para responder cuestiones de carácter general que puedan surgir a cualquier alumno/a de otras facultades de la UMA, ya que existen determinadas cuestiones que son de interés para el conjunto del alumnado universitario.</p>
                 </div>
             </div>
+            <h2 class="text-center" style="margin: 100px"><strong>¿ Donde puedes encontrarnos ?</strong></h2> 
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3198.308949404649!2d-4.47983668486812!3d36.7151406801831!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd72f74b9f3606f1%3A0x9fa32cc2e6b0bbf9!2sE.T.S.+Ingenier%C3%ADa+Inform%C3%A1tica.+Universidad+de+M%C3%A1laga!5e0!3m2!1ses!2ses!4v1557700216191!5m2!1ses!2ses" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>                 
             <div class="row ">  
                 <div class="col-sm-12">
-                <h2 class="text-center" style="margin-top: 100px"><strong>Facultades colaboradoras con DR.GOU</strong></h2> 
+                <h2 class="text-center" style="margin: 100px"><strong>Facultades colaboradoras con DR.GOU</strong></h2> 
                 </div>
             </div>
             
@@ -82,13 +84,9 @@
                         <li><p class="text-center" style="margin-top: 10px"><strong>FEST</strong></p></li>
                         <li><p class="text-center" style="margin-top: 10px"><strong>Facultad de Derecho</strong></p></li>
                     </ul>
-                <h2 class="text-center" style="margin-top: 100px"><strong>¿ Donde puedes encontrarnos ?</strong></h2> 
+                
                 </div>
-            </div>
-            <div class="row"  >  
-                <div class="col-sm-12">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3198.308949404649!2d-4.47983668486812!3d36.7151406801831!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd72f74b9f3606f1%3A0x9fa32cc2e6b0bbf9!2sE.T.S.+Ingenier%C3%ADa+Inform%C3%A1tica.+Universidad+de+M%C3%A1laga!5e0!3m2!1ses!2ses!4v1557700216191!5m2!1ses!2ses" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>                    </div>
-            </div>   
+            </div>  
         </div>
     </div>
         
