c1b67940399be0367d265bc4ae08e31195cb2d2f
alexSsedano <44229620+alexSsedano@users.noreply.github.com>
2019-05-14 16:15:57 +0200

    c

--- src/components/chatBody.vue
+++ src/components/chatBody.vue
@@ -1,10 +1,13 @@
 <template >
-    <div class="chatBody " >
+    <div class="chatBody" >
         <div class="anti" >
             <div class="w-100 h-100">
             <div class="row w-100 antim h-100"  >
                 <div class="col-sm-2 h-100 cg" >
-                    <button type="button " style="margin-top:10px; " class="btn btn-primary btn w-100">Añadir chat</button>
+                    <button type="button " style="margin-top:10px; " @click="changeAdd" class="btn btn-primary btn w-100">Añadir chat</button>
+                    <div v-if="this.add">
+                        <input  v-model="newChatText" type="text" class="w-100" style="margin-top:10px;">
+                    </div>
                 </div>
                 <div class="col-sm-10"></div>
             </div>
@@ -13,8 +16,31 @@
     </div>
 </template>
 <script>
+import firebase from "firebase";
     export default {  
         name: "chatBody",
+        data: function() {
+            return {
+                add: false,
+                newChatText: '',
+            };
+        },
+        methods: {
+            changeAdd: function(){
+                if(this.add){
+                    if(this.newChatText!=''){
+                        console.log(this.newChatText)
+                        firebase.database().ref("newChat/").push({
+                            newChat: this.newChatText
+                        });
+                    this.newChatText = "";
+                    }
+                    this.add = false
+                }else{
+                    this.add = true
+                }
+            }
+        }
     }
 </script>
 <style>
